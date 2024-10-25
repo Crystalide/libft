c785433089bbb91d6fdfb287cb04ba2c549dd4ca
Nastasia Dufourneau <ndufourn@f1r1s8.codam.nl>
2024-10-25 19:02:03 +0200

    commit

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: ndufourn <ndufourn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 19:11:46 by ndufourn          #+#    #+#             */
-/*   Updated: 2024/10/25 18:14:30 by ndufourn         ###   ########.fr       */
+/*   Updated: 2024/10/25 18:59:04 by ndufourn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,57 +14,40 @@
 
 static int	count_words(const char *str, char delimiter);
 static char	*get_word(const char **str, char delimiter);
-void		ft_print_array(char **str, int size, int i);
+static int	fill_str(char **split_str, const char *str, char delimiter);
+// void		ft_print_array(char **str, int size, int i);
 
-int	main(void)
-{
-	char	*str = "123Hello 123";
-	// char	*str = NULL;
+// int	main(void)
+// {
+// 	char	*str = "123Hello 123";
+// 	// char	*str = NULL;
+// 	if (str == NULL || *str == '\0')
+// 	{
+// 		printf("NULL or empty string.\n");
+// 		return (1);
+// 	}
+// 	int		size = count_words(str, ' ');
+// 	char	**test1 = ft_split(str, ' ');
 
-	if (str == NULL)
-	{
-		printf("NULL string not allowed.\n");
-		return (1);
-	}
-	int		size = count_words(str, ' ');
-	char	**test1 = ft_split(str, ' ');
+// 	ft_print_array(test1, size, 0);
+// 	free(test1);
+// 	return (0);
+// }
 
-	ft_print_array(test1, size, 0);
-	free(test1);
-	return (0);
-}
-
-char	**ft_split(char const *str, char c)
+char	**ft_split(char const *str, char delimiter)
 {
 	int		word_count;
-	int		word_index;
 	char	**split_str;
 
-	word_index = 0;
 	if (str == NULL)
 		return (NULL);
-	word_count = count_words(str, c);
+	word_count = count_words(str, delimiter);
 	split_str = (char **)malloc((word_count + 1) * sizeof(char *));
 	if (split_str == NULL)
 		return (NULL);
-	while (*str != '\0')
-	{
-		while (*str == c)
-			str++;
-		if (*str != '\0')
-		{
-			split_str[word_index] = get_word(&str, c);
-			if (split_str[word_index] == NULL)
-			{
-				while (word_index--)
-					free(split_str[word_index]);
-				free(split_str);
-				return (NULL);
-			}
-			word_index++;
-		}
-	}
-	split_str[word_index] = NULL;
+	if (fill_str(split_str, str, delimiter) == 1)
+		return (NULL);
+	split_str[word_count] = NULL;
 	return (split_str);
 }
 
@@ -99,29 +82,49 @@ static char	*get_word(const char **str, char delimiter)
 	return (ft_substr(start, 0, *str - start));
 }
 
-void	ft_print_array(char **str, int size, int i)
+static int	fill_str(char **split_str, const char *str, char delimiter)
 {
-	char	**test1;
+	int	word_index;
 
-	if (size == 0)
+	word_index = 0;
+	while (*str != '\0')
 	{
-		printf("Empty string.\n");
-		return ;
-	}
-	if (i >= size)
-		return ;
-	test1 = ft_split(str[i], ' ');
-	if (test1 == NULL)
-		printf("Memory not allocated.\n");
-	else
-	{
-		printf("Memory successfully allocated.\n"
-			"Before split: %s\nAfter split: ", str[i]);
-		if (test1[0] != NULL)
+		while (*str == delimiter)
+			str++;
+		if (*str != '\0')
 		{
-			printf("%s\n", test1[0]);
+			split_str[word_index] = get_word(&str, delimiter);
+			if (split_str[word_index] == NULL)
+			{
+				while (word_index--)
+					free(split_str[word_index]);
+				free(split_str);
+				return (1);
+			}
+			word_index++;
 		}
 	}
-	free(test1);
-	ft_print_array(str, size, i + 1);
+	return (0);
 }
+
+// void	ft_print_array(char **str, int size, int i)
+// {
+// 	char	**test1;
+
+// 	if (i >= size)
+// 		return ;
+// 	test1 = ft_split(str[i], ' ');
+// 	if (test1 == NULL)
+// 		printf("Memory not allocated.\n");
+// 	else
+// 	{
+// 		printf("Memory successfully allocated.\n"
+// 			"Before split: %s\nAfter split: ", str[i]);
+// 		if (test1[0] != NULL)
+// 		{
+// 			printf("%s\n", test1[0]);
+// 		}
+// 	}
+// 	free(test1);
+// 	ft_print_array(str, size, i + 1);
+// }
